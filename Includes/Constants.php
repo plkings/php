58af66b16757af91d47cf860bc2f73906770172f
Angelo Biaggi <biagaa@Angelos-MacBook-Pro.local>
2019-01-16 19:45:10 -0500

    First commit with databse constants

---
+++ Includes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by Angelo Biaggi
+ * Date: 01/05/19
+ * This file will connect to the database and will return a connection link
+ */
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD','root');
+define('DB_HOST','localhost');
+define('DB_NAME','BarsDB');
